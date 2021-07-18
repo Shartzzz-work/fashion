f4c4965952b48d04d15923ca43c2612b8373ed16
Shartzzz-work <workshartzzz@gmail.com>
2021-07-18 11:57:04 +0300

    shop

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/script.js");
   eleventyConfig.setBrowserSyncConfig({
     files: 'dist/*.css'
   });
+  eleventyConfig.addWatchTarget("src/script.js");
 
   return {
     templateFormats: ["html", "njk"],
